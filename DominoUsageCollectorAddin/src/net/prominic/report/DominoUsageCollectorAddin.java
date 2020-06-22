085e03ce7923a9fd6ff8ddfa654605b1e1500828
Dmytro <dpastov@gmail.com>
2020-06-22 15:02:40 +0200

    initial commit - hello world

---
+++ DominoUsageCollectorAddin/src/net/prominic/report/DominoUsageCollectorAddin.java
@@ -0,0 +1,10 @@
+package net.prominic.report;
+
+import lotus.notes.addins.JavaServerAddin;
+
+public class DominoUsageCollectorAddin extends JavaServerAddin {
+	public void runNotes()
+	{
+		AddInLogMessageText("Hello DominoUsageCollectorAddin", 0);
+	}
+}
